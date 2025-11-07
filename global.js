8dd72e97667edebfbc7635e344a7b35bbae7e03f
cedjj <cejeng@ucsd.edu>
2025-11-07 13:23:09 -0800

    step 0 done

--- global.js
+++ global.js
@@ -13,6 +13,7 @@ let pages = [
   { url: 'contact/', title: 'Contact'},
   { url: 'projects/', title: 'Projects'},
   { url: 'resume/', title: 'Resume'},
+  { url: 'meta/', title: 'Meta'},
   { url: 'https://github.com/cedjj', title: 'Github Profile'}
 ];
 
